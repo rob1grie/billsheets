51acba000d08a0c2cc776f6bef8bfd86e8ff7cf1
Rob Grieshaber <rob@lgd-it.us>
2017-04-13 10:34:58 -0500

    Adding .env to repository Added updated database backup

---
+++ mysql_backups/lgditus_bills_170413.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 13, 2017 at 05:31 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lgditus_bills`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills`
+--
+
+CREATE TABLE `bills` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `payee_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `date_due` date NOT NULL,
+  `amount_due` decimal(7,2) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `amount_paid` decimal(7,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1),
+('2016_08_15_172854_create_payees_and_bills_tables', 1),
+('2016_08_15_210621_create_settings_table', 2),
+('2016_08_18_180316_remove_repeat_interval_from_payees', 3),
+('2016_08_19_154928_delete_settings_table', 4),
+('2017_04_10_053653_create_paydays_table', 5),
+('2017_04_11_024001_add_deleted_field_to_bills_table', 5),
+('2017_04_13_042831_add_amount_paid_to_bills_table', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paydays`
+--
+
+CREATE TABLE `paydays` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `start_date` date NOT NULL,
+  `frequency` int(11) NOT NULL,
+  `amount` decimal(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `paydays`
+--
+
+INSERT INTO `paydays` (`id`, `start_date`, `frequency`, `amount`, `created_at`, `updated_at`) VALUES
+(1, '2017-03-24', 2, '3000.00', '2017-04-13 20:31:18', '2017-04-13 20:31:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payees`
+--
+
+CREATE TABLE `payees` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `day_of_month` int(11) NOT NULL,
+  `repeating` tinyint(1) NOT NULL,
+  `default_amount` decimal(7,2) DEFAULT NULL,
+  `account_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `payment_method` enum('Manual','Auto') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `payees`
+--
+
+INSERT INTO `payees` (`id`, `name`, `day_of_month`, `repeating`, `default_amount`, `account_number`, `payment_method`, `created_at`, `updated_at`) VALUES
+(1, 'Monahan, Gleichner and Wunsch', 7, 0, '388.40', '5297736267688786', 'Auto', NULL, NULL),
+(6, 'Powlowski, Gorczany and Hoppe', 22, 0, '413.69', '342210114681218', 'Auto', NULL, NULL),
+(7, 'Lakin, Ratke and Price', 20, 1, '247.20', '4716288348490580', 'Auto', NULL, NULL),
+(11, 'Chase Mortgage', 2, 0, '1560.56', '', 'Manual', '2016-08-18 21:42:42', '2016-08-18 21:42:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bills`
+--
+ALTER TABLE `bills`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bills_payee_id_foreign` (`payee_id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `paydays`
+--
+ALTER TABLE `paydays`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payees`
+--
+ALTER TABLE `payees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bills`
+--
+ALTER TABLE `bills`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `paydays`
+--
+ALTER TABLE `paydays`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `payees`
+--
+ALTER TABLE `payees`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_payee_id_foreign` FOREIGN KEY (`payee_id`) REFERENCES `payees` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
